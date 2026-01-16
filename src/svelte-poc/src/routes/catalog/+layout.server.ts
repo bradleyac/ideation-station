f52111bac9b2461f23a97f5dca84277e250a1e92
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-01-16 15:46:06 -0500

    Add clientPrincipal check

--- src/svelte-poc/src/routes/catalog/+layout.server.ts
+++ src/svelte-poc/src/routes/catalog/+layout.server.ts
@@ -1,7 +1,17 @@
 import { db } from "$lib/db.svelte";
+import { dev } from "$app/environment";
+import { redirect } from "@sveltejs/kit";
 
-export async function load({ depends, params }) {
+export async function load({ depends, params, platform }) {
   depends("data:ideas");
+
+  if (!dev) {
+    if (!platform?.clientPrincipal) {
+      redirect(302, '/.auth/login/aad');
+    }
+  }
+
+  console.log("Platform info in layout.server.ts:", platform);
 
   return {
     ideas: await db.getAllIdeas(),
