08a0a0e3ba8dc03f9a3a2268f03ae1d0fd9eb43a
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-02-18 10:02:45 -0500

    Redirect if client doesn't have StaticWebAppsAuthCookie

--- src/svelte-poc/src/hooks.client.ts
+++ src/svelte-poc/src/hooks.client.ts
@@ -2,12 +2,13 @@ import { redirect } from "@sveltejs/kit";
 
 const native_fetch = window.fetch;
 
-export async function init() {
-  window.fetch = async (req, init) => {
-    const result = await native_fetch(req, init);
-    if (result.status === 401) {
-      redirect(302, '/.auth/login/aad?post_login_redirect_uri=.referrer'); // Redirect to Azure AD login
+export function init() {
+  window.fetch = (req, init) => {
+    if (req instanceof Request) {
+      if (!req.headers.get('StaticWebAppsAuthCookie')) {
+        redirect(302, '/.auth/login/aad?post_login_redirect_uri=.referrer'); // Redirect to Azure AD login
+      }
     }
-    return result;
+    return native_fetch(req, init);
   };
 }
