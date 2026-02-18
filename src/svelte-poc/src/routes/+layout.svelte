c2e590c66887932e28dd378e4bbe9340e48d00c3
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-02-18 17:19:34 -0500

    Add categories menu

--- src/svelte-poc/src/routes/+layout.svelte
+++ src/svelte-poc/src/routes/+layout.svelte
@@ -4,13 +4,15 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import Spinner from '$lib/components/Spinner.svelte';
 	import { getAllCatalogMetadata, getCatalog } from '$lib/remotes/catalog.remote';
-	import { getCategory } from '$lib/remotes/category.remote';
+	import { getAllCategoryMetadata, getCategory } from '$lib/remotes/category.remote';
 	import { getIdea } from '$lib/remotes/idea.remote';
 	import '../app.css';
 
 	let { children } = $props();
 
-	const route = $derived(page.route);
+	let routeId = $derived(page.route.id);
+	let url = $derived(page.url);
+
 	const getCatalogPromise = $derived(
 		page.params.catalogId ? getCatalog(page.params.catalogId) : undefined
 	);
@@ -32,8 +34,15 @@
 			.catch(console.error);
 	});
 
-	// Invalidate the catalogs list whenever navigation occurs to ensure it doesn't stay open.
-	let invalidationKey = $state(0);
+	let categories = $state<{ id: string; name: string }[] | undefined>(undefined);
+	$effect(() => {
+		if (page.params.catalogId) {
+			getAllCategoryMetadata(page.params.catalogId)
+				.then((metadata) => (categories = metadata))
+				.catch(console.error);
+		}
+	});
+
 	let navigating = $state(false);
 
 	beforeNavigate(() => {
@@ -41,7 +50,6 @@
 	});
 
 	afterNavigate(() => {
-		invalidationKey += 1;
 		navigating = false;
 	});
 </script>
@@ -55,17 +63,20 @@
 		class="p-3 h-(--nav-height) content-center sticky top-0 z-1 bg-gray-50 dark:bg-gray-900 shadow-black/30 shadow-xs"
 	>
 		<ul class="p-0 m-0 list-none flex gap-4 relative">
-			<li class="block border-black">
-				{#if route.id === '/'}
-					<span class="border-b-2 border-eucalyptus-500">Home</span>
-				{:else}
-					<a href="/">Home</a>
-				{/if}
-			</li>
-			<li class="flex gap-2 last:overflow-hidden block border-black">
-				{#key invalidationKey}
+			{#key url.pathname}
+				<li class="block border-black">
+					{#if routeId === '/'}
+						<span class="border-b-2 border-eucalyptus-500">Home</span>
+					{:else}
+						<a href="/">Home</a>
+					{/if}
+				</li>
+				<li class="flex gap-2 block border-black">
 					<div class="group">
-						<a href="/catalogs">Idea Catalogs</a>
+						<a
+							class={[routeId === '/catalogs' && 'border-b-2 border-eucalyptus-500']}
+							href="/catalogs">Idea Catalogs</a
+						>
 						{#if catalogs}
 							<ul
 								class="hidden absolute rounded-sm group-hover:flex z-1 flex-col gap-1 p-2 bg-gray-100 dark:bg-gray-800"
@@ -82,22 +93,41 @@
 							</ul>
 						{/if}
 					</div>
-				{/key}
-				{#if route.id?.startsWith('/catalogs/')}
-					/
-					{#if route.id === '/catalogs/[catalogId]'}
-						<span class="border-b-2 border-eucalyptus-500">{catalogName}</span>
-					{:else}
-						<a href="/catalogs/{page.params.catalogId}">{catalogName}</a>
+					{#if routeId?.startsWith('/catalogs/')}
 						/
-						{#if route.id === '/catalogs/[catalogId]/categories/[categoryId]'}
-							<span class="border-b-2 border-eucalyptus-500">Category: {categoryName}</span>
-						{:else if route.id === '/catalogs/[catalogId]/ideas/[ideaId]'}
-							<span class="border-b-2 border-eucalyptus-500">Idea: {ideaName}</span>
+						<div class="group">
+							<a
+								class={[routeId === '/catalogs/[catalogId]' && 'border-b-2 border-eucalyptus-500']}
+								href="/catalogs/{page.params.catalogId}">{catalogName}</a
+							>
+							{#if categories}
+								<ul
+									class="hidden absolute rounded-sm group-hover:flex w-max z-1 flex-col gap-1 p-2 bg-gray-100 dark:bg-gray-800"
+								>
+									{#each categories as category (category.id)}
+										<li class="w-full flex">
+											<a
+												inert={navigating}
+												class="p-2 rounded-sm w-full hover:bg-gray-200 dark:hover:bg-gray-700"
+												href="/catalogs/{page.params.catalogId}/categories/{category.id}"
+												>{category.name}</a
+											>
+										</li>
+									{/each}
+								</ul>
+							{/if}
+						</div>
+						{#if routeId?.startsWith('/catalogs/[catalogId]/')}
+							/
+							{#if routeId === '/catalogs/[catalogId]/categories/[categoryId]'}
+								<span class="border-b-2 border-eucalyptus-500">Category: {categoryName}</span>
+							{:else if routeId === '/catalogs/[catalogId]/ideas/[ideaId]'}
+								<span class="border-b-2 border-eucalyptus-500">Idea: {ideaName}</span>
+							{/if}
 						{/if}
 					{/if}
-				{/if}
-			</li>
+				</li>
+			{/key}
 		</ul>
 	</nav>
 	<div class="h-[calc(100%_-_calc(var(--spacing)_*_var(--nav-height)_*_1.5))]">
