aee83a23ffb40e3b2f95386c6f45b65899618312
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-02-18 11:18:44 -0500

    Try to redirect in handleError

--- src/svelte-poc/src/hooks.client.ts
+++ src/svelte-poc/src/hooks.client.ts
@@ -1,4 +1,5 @@
-import { redirect } from "@sveltejs/kit";
+import { page } from "$app/state";
+import { redirect, type HandleClientError } from "@sveltejs/kit";
 
 const native_fetch = window.fetch;
 
@@ -11,4 +12,13 @@ export function init() {
     }
     return native_fetch(req, init);
   };
+}
+
+export const handleError: HandleClientError = ({ status, message }) => {
+  if (status === 401) {
+    redirect(302, `/.auth/login/aad?post_login_redirect_uri=${page.url}`); // Redirect to Azure AD login
+  }
+  else {
+    return { message }
+  }
 }
