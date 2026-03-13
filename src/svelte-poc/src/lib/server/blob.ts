8bcf45700647555c3a697da5ba5f729b3de92e3f
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-03-13 15:15:47 -0400

    Try to fix SSR build run

--- src/svelte-poc/src/lib/server/blob.ts
+++ src/svelte-poc/src/lib/server/blob.ts
@@ -5,12 +5,15 @@ class Blob {
   private accountName: string = env.AZURE_BLOB_STORAGE_ACCOUNT_NAME || '';
   private connectionString: string = env.AZURE_BLOB_STORAGE_CONNECTION_STRING || '';
   private containerName = 'idea-images';
-  private client: BlobServiceClient;
+  private client?: BlobServiceClient;
   constructor() {
-    this.client = BlobServiceClient.fromConnectionString(this.connectionString);
+    if (this.connectionString) {
+      this.client = BlobServiceClient.fromConnectionString(this.connectionString);
+    }
   }
 
   public async uploadImage(file: File): Promise<{ key: string, url: string }> {
+    if (!this.client) throw new Error('Client not intialized');
     const id = crypto.randomUUID();
     const extension = file.name.substring(file.name.lastIndexOf('.')) || '.img';
     const blobKey = `${id}${extension}`;
